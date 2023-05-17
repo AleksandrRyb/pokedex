a9fc7b2f94abc2d89674872a8497ce4be7104355
AleksandrRyb <inkognito19931@gmail.com>
2023-05-17 16:25:09 +0300

    pokemon service have added

---
+++ redux/services/pokemon-api.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+export const pokemonApi = createApi({
+  reducerPath: 'pokemonApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+  }),
+  endpoints: (builder) => ({
+    getPokemons: builder.query<any, null>({
+      query: () => 'pokemonList',
+    }),
+  }),
+});
