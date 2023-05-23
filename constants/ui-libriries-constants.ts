8e2113e1880bd2a5865df9cccf242c589e929fc9
AleksandrRyb <inkognito19931@gmail.com>
2023-05-23 09:38:00 +0300

    add styles to customStyles for react-select

--- constants/ui-libriries-constants.ts
+++ constants/ui-libriries-constants.ts
@@ -1,27 +1,29 @@
+import type { CSSObjectWithLabel } from 'react-select';
+
 export const customStyles = {
-  control: (provided, state) => ({
-    ...provided,
+  control: (base: CSSObjectWithLabel, props: any) => ({
+    ...base,
     padding: '2px 0 2px 4px',
     borderWidth: '1px',
     borderRadius: '0.375rem',
-    borderColor: state.isFocused ? 'none' : 'rgb(209, 213, 219)',
-    boxShadow: state.isFocused
+    borderColor: props.isFocused ? 'none' : 'rgb(209, 213, 219)',
+    boxShadow: props.isFocused
       ? '0px 0px 0px 3px rgba(234, 179, 8, 1)'
       : 'none',
     '&:hover': {
       borderColor: 'none',
     },
   }),
-  option: (provided, state) => ({
-    ...provided,
-    backgroundColor: state.isFocused ? 'gray-200' : 'white',
+  option: (base: CSSObjectWithLabel, props: any) => ({
+    ...base,
+    backgroundColor: props.isFocused ? 'gray-200' : 'white',
     color: 'black',
     '&:hover': {
       backgroundColor: 'rgba(234, 179, 8, 0.6)',
     },
   }),
-  placeholder: (provided) => ({
-    ...provided,
+  placeholder: (base: CSSObjectWithLabel) => ({
+    ...base,
     color: 'rgba(0,0,0, 0.4)', // Change the color to your desired value
   }),
 };
