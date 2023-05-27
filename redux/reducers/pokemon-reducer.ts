a901cb7cc85ec97f0397f4df7a63556af781b95d
AleksandrRyb <inkognito19931@gmail.com>
2023-05-27 14:08:14 +0300

    add pokemon reducer type

--- redux/reducers/pokemon-reducer.ts
+++ redux/reducers/pokemon-reducer.ts
@@ -1,7 +1,30 @@
 /* eslint-disable no-param-reassign */
 import { createReducer } from '@reduxjs/toolkit';
 
+import type { NameUrlPair } from '../../types/Pokemon';
 import * as pokemonActions from '../actions/pokemon-actions';
+
+type PokemonReducer = {
+  isPokemonListLoading: boolean;
+  error: ReturnType<typeof Error> | null;
+  list: NameUrlPair[] | [];
+
+  isPaginateByLocalData: boolean;
+
+  isGetPokemonByName: boolean;
+  isGetPokemonByType: boolean;
+
+  filters: {
+    pokemonName: string;
+    pokemonType: string | null;
+  };
+
+  paginationData: {
+    offset: number;
+    limit: number;
+    currentPage: number;
+  };
+};
 
 const initialState = {
   isPokemonListLoading: false,
@@ -25,17 +48,20 @@ const initialState = {
   },
 };
 
-export const pokemonReducer = createReducer(initialState, (builder) => {
-  builder
-    .addCase(pokemonActions.requestPokemonsList, (state) => {
-      state.isPokemonListLoading = true;
-    })
-    .addCase(pokemonActions.requestPokemonsListSuccess, (state, action) => {
-      state.isPokemonListLoading = false;
-      state.list = action.payload.list;
-    })
-    .addCase(pokemonActions.requestPokemonsListError, (state, action) => {
-      state.isPokemonListLoading = false;
-      state.error = action.payload.error;
-    });
-});
+export const pokemonReducer = createReducer<PokemonReducer>(
+  initialState,
+  (builder) => {
+    builder
+      .addCase(pokemonActions.requestPokemonsList, (state) => {
+        state.isPokemonListLoading = true;
+      })
+      .addCase(pokemonActions.requestPokemonsListSuccess, (state, action) => {
+        state.isPokemonListLoading = false;
+        state.list = action.payload.list;
+      })
+      .addCase(pokemonActions.requestPokemonsListError, (state, action) => {
+        state.isPokemonListLoading = false;
+        state.error = action.payload.error;
+      });
+  }
+);
